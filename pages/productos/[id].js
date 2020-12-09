7f18e9d66c3ed96cb2f6da529353c7c264e0bd0c
ysaiascampos <ysaiascampos@gmail.com>
2020-12-09 20:49:19 -0300

    sss

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -158,6 +158,36 @@ const Producto = () => {
         }
     }
 
+    // función que revisa que el creador del producto sea el mismo que esta autenticado
+    const puedeBorrar = () => {
+        if(!usuario) return false;
+
+        if(creador.id === usuario.uid) {
+            return true
+        }else{
+            return false;
+        }
+    }
+
+    // elimina un producto de la bd
+    const eliminarProducto = async () => {
+
+        if(!usuario) {
+            return router.push('/login')
+        }
+
+        if(creador.id !== usuario.uid) {
+            return router.push('/')
+        }
+
+        try {
+            await firebase.db.collection('productos').doc(id).delete();
+            router.push('/')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return ( 
         <Layout>
             <>
@@ -227,6 +257,12 @@ const Producto = () => {
                             </TextVotos>                        
                         </aside>
                     </ContenedorProducto>
+                    {
+                        puedeBorrar() && 
+                        <Boton
+                            onClick={eliminarProducto}
+                        >Eliminar Producto</Boton>
+                    }
                 </div>
             ) }
             </>
